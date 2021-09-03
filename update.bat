574bf75f0f7dd245d1cca5b840bc8be315d93fa7
misersun <miser@gmail.com>
2021-09-03 15:45:38 +0800

    update

--- update.bat
+++ update.bat
@@ -3,7 +3,7 @@ d:
 cd D:\free\github\company001
 cd
 copy D:\free\github\config_v.yaml . /Y
-copy D:\free\github\\config_other.yaml . /Y
+copy D:\free\github\config_other.yaml . /Y
 git add .
 git commit -m "update"
 git branch -M main
