c09d0fa65bbacf39973c3213f9772b859ffc5e46
misersun <misersun@gmail.com>
2021-09-03 16:13:31 +0800

    first commit

--- update.bat
+++ update.bat
@@ -1,9 +1,9 @@
 cd
-d:
-cd D:\free\github\company001
+c:
+cd c:\github\config002
 cd
-copy D:\free\github\config_v.yaml . /Y
-copy D:\free\github\config_other.yaml . /Y
+copy c:\github\config_v.yaml . /Y
+copy c:\github\config_other.yaml . /Y
 git add .
 git commit -m "update"
 git branch -M main
