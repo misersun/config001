04c0b7971fa1052bbe1aee0d048ad148f49e2869
misersun <miser@gmail.com>
2021-09-03 15:00:47 +0800

    update

--- update.bat
+++ update.bat
@@ -2,8 +2,8 @@ cd
 d:
 cd D:\free\github\company001
 cd
-copy E:\FangCloudSync\Python\workspace\PycharmProjects\myapp\AutoClashYaml\config_v.yaml . /Y
-copy E:\FangCloudSync\Python\workspace\PycharmProjects\myapp\AutoClashYaml\config_other.yaml . /Y
+copy D:\free\github\config_v.yaml . /Y
+copy D:\free\github\\config_other.yaml . /Y
 git add .
 git commit -m "update"
 git branch -M main
